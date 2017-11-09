d5f9b8ba5ac93147eb023d64ba5ac10d77ec2206
Max Prettyjohns <heshoots9999@gmail.com>
2017-11-09 11:17:26 +0000

    Add basic kubernetes deployment

--- nifi-node/start-nifi.sh
+++ nifi-node/start-nifi.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
+fqhn=$(hostname -i | sed -e "s/\./-/g").default.pod.cluster.local
+
 sed -i -e \
-  "s|^nifi.web.http.host=.*$|nifi.web.http.host=$(hostname)|" \
+  "s|^nifi.web.http.host=.*$|nifi.web.http.host=$fqhn|" \
   conf/nifi.properties
 sed -i -e \
-  "s|^nifi.cluster.node.address=.*$|nifi.cluster.node.address=$(hostname)|" \
+  "s|^nifi.cluster.node.address=.*$|nifi.cluster.node.address=$fqhn|" \
   conf/nifi.properties
 sed -i -e \
-  "s|^nifi.remote.input.host=.*$|nifi.remote.input.host=$(hostname)|" \
+  "s|^nifi.remote.input.host=.*$|nifi.remote.input.host=$fqhn|" \
   conf/nifi.properties
 
 bin/nifi.sh run
